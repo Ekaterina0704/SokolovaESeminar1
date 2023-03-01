3bab12471b96dbd6120387fb5cf01a61ba664ef9
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-01 18:33:25 +0300

    Задача 7

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem1Zadacha7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+if (number < 0 ) number = -number;
+if(number>99 && number<1000)
+{
+    int poslchislo = number%10;
+    Console.Write($"Последняя цифра числа = {poslchislo}");
+}
