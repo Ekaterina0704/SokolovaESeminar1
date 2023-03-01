7cc2c73027665110b6d8f0fd9b34906251a78510
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-01 18:31:46 +0300

    Задача 5

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem1Zadacha5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите натуральное число:");
+int number=Convert.ToInt32(Console.ReadLine());
+int Chetchic = -number;
+if (number>0)
+{
+     while (Chetchic<=number)
+    {
+        Console.Write(Chetchic);
+        Chetchic=Chetchic+1; 
+    }
+}
+else
+{
+  Console.Write("Введуно ненатуральное число:");  
+}
+
