3ddee602fa7dfc3c9f466139eda257697405e36c
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-01 18:28:35 +0300

    Задача 1.1

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem1Zadacha1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите 1е число:");
+int number1= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2е число:");
+int number2= Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2*number2)
+{  Console.Write("Да, 1е число явл квадратом второго");  
+}
+else       
+{
+    Console.Write("Нет, 1е число не явл квадратом второго");  
+}
