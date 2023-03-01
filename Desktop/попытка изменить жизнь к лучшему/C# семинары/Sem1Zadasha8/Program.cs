ed69901abe67d452fe18af27e5fe6ec026c7a246
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-01 18:34:08 +0300

    Задача 8

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem1Zadasha8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+if (number>0)
+{
+    while (a<=number)
+    {
+        Console.Write(a);
+        a=a+2;
+    }
+}
+else
+{
+    Console.Write("Введено ненатуральное число");
+}
