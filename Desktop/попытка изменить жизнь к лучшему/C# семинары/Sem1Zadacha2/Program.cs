f45e25bd1d0543d2053a1f9aa5e2c30f4c310604
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-01 18:29:17 +0300

    Задача 2

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem1Zadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1е число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2е число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1==number2) Console.Write("Числа равны");
+else if (number1>number2)
+{
+    Console.WriteLine($"{number1} - наибольшее число, {number2} - наименьшее число");
+}
+else
+{
+    Console.WriteLine($"{number2} - наибольшее число, {number1} - наименьшее число");  
+}
