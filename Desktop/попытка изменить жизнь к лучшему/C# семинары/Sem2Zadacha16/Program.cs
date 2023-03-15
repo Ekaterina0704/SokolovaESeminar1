9b0a6b637d693a074e95846a8172e869740f7a4b
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-15 23:21:37 +0300

    задача 16

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zadacha16/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1е число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2е число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1==number2*number2)
+//      Console.WriteLine("Да, одно из чисел является квадратом второго");
+// else if (number2==number1*number1)
+//      Console.WriteLine("Да, одно из чисел является квадратом второго");
+// else 
+//      Console.WriteLine("Нет, ниодно из чисел не является квадратом второго");
+
+Console.WriteLine(Square(number1, number2)?"ДА":"нет");
+
+bool Square(int num1,int num2)
+{
+    return num1==num2*num2||num2==num1*num1;
+}
