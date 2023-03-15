2d08ab85af9bd44709701cef80dec5626ff2d015
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-15 23:06:33 +0300

    Задача 14

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zadacha14/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 7==0 && number % 23==0)
+//     Console.WriteLine("да");
+// else
+//     Console.WriteLine("нет"); 
+
+Console.WriteLine(Multiply(number)? "Да": "Нет");
+
+bool Multiply(int num)
+{
+    return number % 7==0 && number % 23==0;
+}
+
