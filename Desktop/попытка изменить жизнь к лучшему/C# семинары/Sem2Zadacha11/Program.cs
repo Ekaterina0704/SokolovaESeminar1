b847a654b471fee09a551a9c62c3ae33b93da7ab
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-15 22:08:28 +0300

    Задача 11

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zadacha11/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine($"Рандомное трехзначное число -> {number}");
+// int firstDigit=number/100;
+// int thirdDigit=number%10;
+// Console.Write(firstDigit);
+// Console.WriteLine(thirdDigit);
+int twodigitnumber=Twodigitnumber(number);
+Console.WriteLine($"Составное двузначное число -> {twodigitnumber}"); 
+
+int Twodigitnumber(int num)
+{
+    int firstDigit=num/100;
+    int thirdDigit=num%10;
+    int result=firstDigit*10+thirdDigit;
+    return result;
+}
