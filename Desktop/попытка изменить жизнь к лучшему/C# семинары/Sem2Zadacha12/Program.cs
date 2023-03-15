fcffcb4cd1b2ccbe82c7975a68427f6b3c7c5346
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-15 22:34:04 +0300

    Задача 12

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zadacha12/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 1е число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2е число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int multiplicity=Multiplicity(number1,number2);
+Console.WriteLine(multiplicity==0?"Кратно":$"Некратно, остаток ->{multiplicity}");
+
+int Multiplicity(int num1, int num2)
+{
+    return num1%num2;
+}
