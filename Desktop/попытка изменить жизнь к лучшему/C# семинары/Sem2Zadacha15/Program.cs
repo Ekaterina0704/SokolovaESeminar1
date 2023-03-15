f1dc4cfd54a1d85366b921acc3994f2e24104768
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 00:15:54 +0300

    Задача 15

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zadacha15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0 && number>8)
+Console.WriteLine("Введите корректный день недели");
+else if (number>5)
+Console.WriteLine("Выходной день");
+else
+Console.WriteLine("Рабочий день недели");
