ca4442f22a57d21e6d88d4a0177c950c0ac1ee36
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 00:08:46 +0300

    Задача 13

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem2Zagacha13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100) 
+Console.WriteLine("третьей цифры у числа нет");
+else 
+{
+    while (number>999)
+    {
+        number=number/10;
+    }
+int thirddigit = number%10;
+Console.WriteLine($"Третья цифра числа ->{thirddigit}");
+}
