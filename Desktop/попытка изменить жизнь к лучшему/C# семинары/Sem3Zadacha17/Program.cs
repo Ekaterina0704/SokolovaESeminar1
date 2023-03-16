5401ad193ca8da98a24c23056f2c60ad40b0cd76
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 13:09:31 +0300

    задача 17

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha17/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine($"Введите координаты точки");
+Console.Write("Х: ");
+int xCoord= Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoord= Convert.ToInt32(Console.ReadLine());
+
+int Quarter(int x, int y)
+{
+    if(x>0 && y>0) return 1;
+    if(x>0 && y<0) return 4;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y>0) return 3;
+    return 0;
+}
+
+int quarter=Quarter(xCoord, yCoord);
+string result =  quarter>0
+                ? $"указанные координаты соответствуют {quarter}" 
+                :  "введены некорректные координаты";
+Console.WriteLine(result);     
+
