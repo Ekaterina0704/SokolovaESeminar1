bfcb588921fc0f495637310a4c3e275bc7e2ad47
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 21:12:55 +0300

    Pflfxf 26

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha26/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Count(int num)
+{
+    int digit=0;
+    do
+    {
+        num/=10;
+        digit++;
+    }
+    while (num!=0);
+    return digit;
+}
+
+int count= Count(number);
+Console.WriteLine($"В числе {number} содержится {count} цифр");
