6207702c4ec8c66a0dd01ae4e826a320007f10f3
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 14:22:43 +0300

    задача 22

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha22/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write($"Введите число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+
+void Square(int num)
+{
+    int i=1;
+    int result=1;
+    while (i<=num)
+    {
+        result=i*i;
+        Console.WriteLine($"{i} {result}");
+        i++;
+    }
+}
+
+Square(number);
