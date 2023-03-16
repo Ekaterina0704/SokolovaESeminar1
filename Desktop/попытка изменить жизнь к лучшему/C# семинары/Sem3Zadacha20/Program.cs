67e4f54cdb506f9d162c41d6e1fa0a119416ad8c
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 13:59:42 +0300

    задача 20

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha20/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine($"Введите координаты первой точки");
+Console.Write("Х1: ");
+int x1Coord= Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1Coord= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите координаты второй точки");
+Console.Write("Х2: ");
+int x2Coord= Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2Coord= Convert.ToInt32(Console.ReadLine());
+
+double Distance(int x1,int y1,int x2,int y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+}
+
+double distance=Distance(x1Coord,y1Coord,x2Coord,y2Coord);
+double resRound=Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние между точками в 2D постранстве = {Math.Round(distance, 2, MidpointRounding.ToZero)}");
