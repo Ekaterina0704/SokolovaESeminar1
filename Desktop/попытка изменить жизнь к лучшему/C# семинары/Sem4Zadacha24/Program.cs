bea3b447f43a594d6b2bf9a4513b6c3390c260b0
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 20:35:44 +0300

    задача 24

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha24/Program.cs
@@ -0,0 +1,31 @@
+﻿//cумма чисел от 0 до заданного числа
+Console.Write($"Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+// int Summ(int num)
+// {
+//     int summa=0;
+//     int i = 0;
+//     while (i<=num)
+//     {
+//         summa=summa+i;
+//         i++;
+//     }
+//     return summa;
+// }
+
+// int summ= Summ(A);
+// Console.Write(summ);
+
+int summ= Summ(A);
+Console.Write(summ);
+
+int Summ(int num)
+{
+    int summa=0;
+    for (int i=1; i <= num; i++)
+    {
+        summa=summa+i;
+    }
+    return summa;
+}
