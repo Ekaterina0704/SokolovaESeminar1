629490bd9776b18bd2d9a742bcf7913d77904d0b
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 13:24:17 +0300

    задача 18

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write($"Введите номер четверти: ");
+int quarter= Convert.ToInt32(Console.ReadLine());
+
+string Range(int x)
+{
+    if (x==1) return "(x>0 && y>0)";
+    if (x==4) return "(x>0 && y<0)";
+    if (x==2) return "(x<0 && y>0)";
+    if (x==3) return "(x<0 && y>0)";
+    return "некорректное значение";
+}
+string range = Range(quarter);
+Console.WriteLine($"Заданой четверти соответствует диапазон: {range}");     
