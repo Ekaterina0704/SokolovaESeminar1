cf86f79a75acdd1590246e22cce2771a282ee1d2
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 16:39:41 +0300

    Задача 23

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write($"Введите число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int i=1;
+    int result=0;
+    while (i<=num)
+    {
+        result=i*i*i;
+        Console.WriteLine($"{i} {result}");
+        i++;
+    }
+}
+
+Cube(number);
