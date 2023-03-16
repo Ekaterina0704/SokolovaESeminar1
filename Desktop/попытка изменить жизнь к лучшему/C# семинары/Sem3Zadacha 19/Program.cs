8dc0dc73e0b20f16d684b769e91ebc1f0fb85d4a
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-16 16:23:43 +0300

    задача 19

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem3Zadacha 19/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write($"Введите пятизначное число: ");
+int number= Convert.ToInt32(Console.ReadLine());
+
+bool Fivedigitnumber (int num)
+{
+    return num<=99999 && num>=10000;
+}
+
+bool Palindrome(int num)
+{
+        int a=num/10000;
+        int b=num%10;
+        int c=num/1000%10;
+        int d=num/10%10;
+        return a==b && c==d;
+}
+
+if (Fivedigitnumber(number))
+{
+    Palindrome(number);
+    Console.WriteLine(Palindrome(number)?"Да, число является палиндромом": "Нет, число не является палиндромом");
+}
+else
+Console.Write("Введите пятизначное число");
