0f14b88502f855bb03137496a6db9bc78bca2468
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-17 12:43:14 +0300

    задача 25

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha25/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите 1е число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int num1, int num2)
+{
+    int multiplication = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        multiplication = multiplication * num1;
+    }
+    return multiplication;
+}
+
+bool Naturalnumber(int num2)
+{
+    return num2 > 0;
+}
+
+Console.WriteLine(Naturalnumber(number2) ? Degree(number1, number2) : "Введите положительное число в качестве степени");
