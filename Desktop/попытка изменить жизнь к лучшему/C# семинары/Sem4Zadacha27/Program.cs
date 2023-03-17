528afe1df5ec7e6c3bb9eca985f15768b8564468
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-17 12:51:42 +0300

    Задача 27

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Summaglobal(int num)
+{
+    int summ = 0;
+    do
+    {
+        int remainder = num % 10;
+        summ = summ + remainder;
+        num = num / 10;
+    }
+    while (num != 0);
+    return summ;
+}
+
+int count = Summaglobal(number);
+Console.WriteLine($"В числе {number} cумма цифр = {count}");
