2c7fc2cc94fae24b70129b3aff267c9150c0ec79
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-17 12:24:22 +0300

    задача 30

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha30/Program.cs
@@ -0,0 +1,22 @@
+﻿// задать массив с рандомными числами
+
+int[] array = new int[8];
+
+void FillArray(int[]arr)
+{
+    for (int i=0;i<arr.Length;i++)
+    {
+        arr[i]=new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[]arr)
+{
+    int count = arr.Length;
+    for (int i=0;i< count;i++)
+    {
+        Console.Write($"{arr[i] }");
+    }
+}
+FillArray(array);
+PrintArray(array);
