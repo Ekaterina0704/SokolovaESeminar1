3e1360713e0afb17d372f21cbecd306bff437b07
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-17 11:43:46 +0300

    задача 28

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem4Zadacha28/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// int summ= Summ(A);
+// Console.Write(summ);
+Console.Write($"Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int mult= Mult(A);
+Console.Write(mult);
+
+int Mult(int num)
+{
+    int mult=1;
+    for (int i=1; i <= num; i++)
+    {
+        mult=mult*i;
+    }
+    return mult;
+}
