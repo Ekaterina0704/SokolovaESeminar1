32fd2c7401a39f74ef6dd98398513f98a856f97a
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-22 22:19:49 +0300

    32

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem5Zadacha32/Program.cs
@@ -0,0 +1,37 @@
+﻿// создать массив и заменить элементы на противоположные
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void Replacement (int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i]* -1;
+    }
+}
+
+int[] array = CreateArrayRndInt(7, -10, 10);
+PrintArray(array);
+Replacement(array);
+PrintArray(array);
