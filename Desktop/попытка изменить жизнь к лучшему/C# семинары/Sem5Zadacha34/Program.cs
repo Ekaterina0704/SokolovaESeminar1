f6771f79ee22e8af0c3c747da1a8947157801896
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-22 23:39:31 +0300

    34

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem5Zadacha34/Program.cs
@@ -0,0 +1,39 @@
+﻿// задать массив заполненный произвольными положительными трехзначными числами
+// посчитать количество четных чисел в массиве
+
+int [] Createarray (int size, int min, int max)
+{
+    int [] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i]=rnd.Next(min,max);
+    }
+    return arr;
+}
+
+void PrintArray(int[]arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i<arr.Length-1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int СountingEvenNumbers (int[]arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2==0) count++;
+    }
+    return count;
+}
+
+int [] array = Createarray(7,100,1000);
+PrintArray(array);
+int countingEvenNumbers = СountingEvenNumbers(array);
+Console.WriteLine($"Количество четных чисел в массиве= {countingEvenNumbers}");
