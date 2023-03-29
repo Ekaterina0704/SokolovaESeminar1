8192cf9882e5eba072a1564d2909130e76b7217d
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-29 17:00:21 +0300

    46

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem7Zadacha47/Program.cs
@@ -0,0 +1,32 @@
+﻿// задать двумерный массив заполненный случайными целыми числами
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];//0 и 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 100);
+PrintMatrix(array2d);
