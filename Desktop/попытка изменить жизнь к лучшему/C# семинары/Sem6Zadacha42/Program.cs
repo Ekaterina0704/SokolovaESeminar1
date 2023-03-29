5a99b33a1cd4a91bb2a24a2f4623050c6a850fd3
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-29 14:22:56 +0300

    42

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem6Zadacha42/Program.cs
@@ -0,0 +1,19 @@
+﻿// пеобразование десятичного числа в двоичное
+
+Console.Write("Введите число в десятичной системе счисления:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Translate (int num)
+{
+    int result =0;
+    int discharge = 1;
+    while (num!=0)
+    {
+        result=result+(num%2)*discharge;
+        num/=2;
+        discharge*=10;
+    }
+    return result;
+}
+
+Console.WriteLine($"Число {number} в десятичной системе счисления = {Translate(number)}");
