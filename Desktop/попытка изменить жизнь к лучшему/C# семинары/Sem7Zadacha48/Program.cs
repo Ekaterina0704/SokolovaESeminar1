1b579b76160f2547d88d15fe1015dea4e9f0e11f
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-29 17:13:31 +0300

    48

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem7Zadacha48/Program.cs
@@ -0,0 +1,29 @@
+﻿// задать массив значение каждого элемента равно сумме столбца и строки
+
+int[,] CreateMatrixRndInt(int rows, int colums)
+{
+    int[,] matrix = new int[rows, colums];//0 и 1
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4);
+PrintMatrix(array2d);
