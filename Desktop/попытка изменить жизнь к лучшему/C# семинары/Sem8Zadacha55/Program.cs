712905f6b5048e92aff13216ee98883fa9aa4b4c
Ekaterina0704 <sk0704200036@gmai.com>
2023-03-30 20:07:27 +0300

    55

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem8Zadacha55/Program.cs
@@ -0,0 +1,56 @@
+﻿// задать двумерный массив 
+// заменить строки на столбцы
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];//0 и 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4 }");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Reverse (int[,] matrix)
+{
+    int[,] array = new int[matrix.GetLength(0), matrix.GetLength(1)];//0 и 1
+   
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[i,j]=matrix[j,i]; 
+        }
+    }
+    return array;
+}
+
+// bool Verification(int[,] matrix)
+// {
+//     if (matrix.GetLength(0)=matrix.GetLength(1)) return true;
+//     else return false;
+// }
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int[,]reverse = Reverse(array2d);
+// Console.WriteLine (Verification(array2d) ? $"{reverse}" :"Нет, это невозможно");
+PrintMatrix(reverse);
