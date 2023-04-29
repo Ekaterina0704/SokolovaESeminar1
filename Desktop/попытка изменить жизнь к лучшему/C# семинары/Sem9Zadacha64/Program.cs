660a76021696acef2161a00c1fb0ddb00cca7484
Ekaterina0704 <sk0704200036@gmai.com>
2023-04-29 17:57:17 +0300

    64

---
+++ Desktop/попытка изменить жизнь к лучшему/C# семинары/Sem9Zadacha64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N.
+//  Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Ведите натуральное отличное от 0 число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1)
+    Console.WriteLine("Ввели число не соответсвующее условию");
+else
+{
+    Console.Write($"N = {N} -> ");
+    NaturNumber(N);
+}
+
+void NaturNumber(int n)
+{
+    if (n == 0)
+        return;
+    Console.Write($"{n} ");
+    NaturNumber(n - 1);
+}
